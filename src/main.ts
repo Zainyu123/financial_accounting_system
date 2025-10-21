953ba5f551c9fdf0b1a7a877c092966b27c0c8db
zainyu <2860888942@qq.com>
2025-10-21 09:56:03 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import zhCn from 'element-plus/locale/zh-cn'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import './assets/global.css'
 import router from './router/index'
 
